fd73369df10ae2bbc6645e01394a529b19b1672a
ghamsari-keyan <77293068+ghamsari-keyan@users.noreply.github.com>
2025-03-21 15:10:39 -0400

    separated apps & notifications
    just need on click listener for the apps being pressed from the main page

--- app/src/main/java/com/example/breadcrumbsettings/settingsSubActivities/AppsActivity.java
+++ app/src/main/java/com/example/breadcrumbsettings/settingsSubActivities/AppsActivity.java
@@ -11,17 +11,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.breadcrumbsettings.MainSettingsActivity;
 import com.example.breadcrumbsettings.R;
 
-public class AppsAndNotificationsActivity extends AppCompatActivity {
+public class AppsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_apps_and_notifications);
-
-        // fetch the linear layouts i defined in my source xml
-        LinearLayout notificationsSection = findViewById(R.id.notificationsSection);
-        LinearLayout assistantSection = findViewById(R.id.assistantSection);
-        LinearLayout advancedSection = findViewById(R.id.advancedSection);
+        setContentView(R.layout.activity_apps);
 
         // fetch the toolbar so i can add functionality to the back button
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -31,20 +26,10 @@ public class AppsAndNotificationsActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
-        // set on click listeners for each
-        notificationsSection.setOnClickListener(v -> {
-                    Intent intent = new Intent(AppsAndNotificationsActivity.this, NotificationsActivity.class);
-                    startActivity(intent);
-                });
-        assistantSection.setOnClickListener(v ->
-                Toast.makeText(this, "Placeholder", Toast.LENGTH_SHORT).show());
-        advancedSection.setOnClickListener(v ->
-                Toast.makeText(this, "Placeholder", Toast.LENGTH_SHORT).show());
-
         // set on click listener for the back button being pressed
         toolbar.setNavigationOnClickListener(v -> {
             // send the user back to the main activity
-            Intent intent = new Intent(AppsAndNotificationsActivity.this, MainSettingsActivity.class);
+            Intent intent = new Intent(AppsActivity.this, MainSettingsActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
             startActivity(intent);
             finish(); // Close the current activity
