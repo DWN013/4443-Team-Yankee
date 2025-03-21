46e92937b0194c463aadbb5797e65daaeb94a19d
YuriM21 <yurim21@my.yorku.ca>
2025-03-20 21:11:21 -0400

    Sound activity manifest commented
    commented error in manifest

---
+++ app/src/main/java/com/example/breadcrumbsettings/settingsSubActivities/DisplayActivity.java
@@ -0,0 +1,37 @@
+package com.example.breadcrumbsettings.settingsSubActivities;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.breadcrumbsettings.MainSettingsActivity;
+import com.example.breadcrumbsettings.R;
+
+public class DisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        toolbar.setNavigationOnClickListener(v -> {
+            // send user back to main activity
+            Intent intent = new Intent(DisplayActivity.this, MainSettingsActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            startActivity(intent);
+            finish(); // Close the current activity
+        });
+    }
+}
