f167cbaaf187b96c9964665e05043f99b1c1b125
YuriM21 <yurim21@my.yorku.ca>
2025-03-28 14:16:48 -0400

    Got TouchSensitivityActivity.java to work with breadcrumbs

--- app/src/main/java/com/example/breadcrumbsettings/settingsSubActivities/TouchSensitivityActivity.java
+++ app/src/main/java/com/example/breadcrumbsettings/settingsSubActivities/TouchSensitivityActivity.java
@@ -2,6 +2,7 @@ package com.example.breadcrumbsettings.settingsSubActivities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -28,9 +29,6 @@ public class TouchSensitivityActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_display_touchsensitivity);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
         breadcrumbsViewModel = new ViewModelProvider(this).get(BreadcrumbsViewModel.class);
 
         // Deserialize breadcrumbs if present
@@ -42,14 +40,19 @@ public class TouchSensitivityActivity extends AppCompatActivity {
         breadcrumbsViewModel.addBreadcrumb("Display & touch", DisplayActivity.class);
         breadcrumbsViewModel.addBreadcrumb("Touch Sensitivity", TouchSensitivityActivity.class);
 
+        showBreadcrumbsFragment();
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
-
         toolbar.setNavigationOnClickListener(v -> {
             // send user back to main activity
             Intent intent = new Intent(TouchSensitivityActivity.this, DisplayActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            intent.putExtra("breadcrumbs", breadcrumbsViewModel.serializeBreadcrumbs());
             startActivity(intent);
             finish(); // Close the current activity
         });
